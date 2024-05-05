a33f42dd33aef4b53cade178684b9f7160936691
Comalnik <gostencnikluka@proton.me>
2024-05-05 00:48:56 +0000

    Auto solve captchas

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions 
+from selenium_recaptcha_solver import RecaptchaSolver
 import time
 import random
 import os
